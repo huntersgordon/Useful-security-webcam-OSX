acd5b15260a7c117645b35e9efa83e0a5262382e
gotsteel <huntersgordon@gotsteel.local>
2018-07-11 14:14:34 -0700

    2nd commit

--- motiondetect.fish
+++ motiondetect.fish
@@ -8,6 +8,7 @@ while true
   set x (java ImgDiffPercent);
   set motionDetected (echo $x'>'3 | bc -l)
   if [ $motionDetected -eq 1 ]
+    echo 'motion detected!'
     #imessage -a b.jpg -c "your phone number here"
     echo (date) >> MotionDetectionDates.txt
     cp "b.jpg" motion_detections/(date).jpg
